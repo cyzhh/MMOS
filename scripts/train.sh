ca445238b4daf7a3a326b5d544f1b873a0776915
Yezeng Chen <66300819+cyzhh@users.noreply.github.com>
2023-12-25 21:47:55 +0800

    Add files via upload

---
+++ scripts/train.sh
@@ -0,0 +1,89 @@
+set -ex
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+NUM_GPUS=16
+
+
+# ------------------- model -------------------
+MODEL_SIZE=$2
+if [ "$MODEL_SIZE" = "7b" ]; then
+    LEARNING_RATE=2e-5
+    DEEPSPEED=ds_configs/stage3_offload_optim_accelerate.conf
+    BATCH_SIZE_PER_GPU=8
+elif [ "$MODEL_SIZE" = "13b" ]; then
+    LEARNING_RATE=2e-5
+    DEEPSPEED=ds_configs/stage3_offload_optim_accelerate.conf
+    BATCH_SIZE_PER_GPU=8
+elif [ "$MODEL_SIZE" = "34b" ]; then
+    LEARNING_RATE=1e-5
+    DEEPSPEED=ds_configs/stage3_offload_optim_accelerate.conf
+    BATCH_SIZE_PER_GPU=8
+elif [ "$MODEL_SIZE" = "70b" ]; then
+    LEARNING_RATE=1e-5
+    DEEPSPEED=ds_configs/stage3_offload_optim_accelerate.conf
+    BATCH_SIZE_PER_GPU=4
+else
+    echo "MODEL_SIZE should be 7b, 13b, 34b, or 70b"
+    exit 1
+fi
+
+BASE_MODEL=$1
+if [ "$BASE_MODEL" = "llama2" ]; then
+    MODEL_PATH=/data/pretrain_model/Llama-2-${MODEL_SIZE}-hf
+elif [ "$BASE_MODEL" = "codellama" ]; then
+    MODEL_PATH=/data/pretrain_model/CodeLlama-${MODEL_SIZE}-Python-hf
+elif [ "$BASE_MODEL" = "tora-code" ]; then
+    MODEL_PATH=/data/pretrain_model/tora-${MODEL_SIZE}-code
+else
+    echo "BASE_MODEL should be llama2 or codellama"
+    exit 1
+fi
+
+
+# ------------------- data ------------------
+DATA_NAME="mix11"
+NUM_TRAIN_EPOCHS=1
+STEP=800
+JOB_NAME=${DATA_NAME}_ep${NUM_TRAIN_EPOCHS}
+
+TOTAL_BATCH_SIZE=128
+GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
+echo "Training llama model ${MODEL_SIZE} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
+
+TRAIN_FILE=train_data/${DATA_NAME}/train.jsonl
+OUTPUT_DIR=train_outputs/${BASE_MODEL}_${MODEL_SIZE}/${JOB_NAME}
+mkdir -p $OUTPUT_DIR
+
+DEEPSPEED_HOSTFILE=hostfile
+
+accelerate launch \
+    --mixed_precision bf16 \
+    --num_machines 2 \
+    --num_processes $NUM_GPUS \
+    --use_deepspeed \
+    --deepspeed_hostfile $DEEPSPEED_HOSTFILE \
+    --deepspeed_multinode_launcher pdsh \
+    --machine_rank 1  --main_process_ip 36.134.151.134  --main_process_port 17200 \
+    --deepspeed_config_file $DEEPSPEED \
+    train/finetune.py \
+    --model_name_or_path ${MODEL_PATH} \
+    --use_slow_tokenizer \
+    --gradient_checkpointing \
+    --train_file $TRAIN_FILE \
+    --max_seq_length 2048 \
+    --preprocessing_num_workers 16 \
+    --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
+    --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
+    --learning_rate $LEARNING_RATE \
+    --lr_scheduler_type linear \
+    --warmup_ratio 0.03 \
+    --weight_decay 0. \
+    --max_train_steps 100 \
+    --output_dir $OUTPUT_DIR \
+    --with_tracking \
+    --report_to tensorboard \
+    --logging_steps 1 \
+    --use_flash_attn \
+    --mask_prompt \
+    --seed 0 \
+    | tee $OUTPUT_DIR/logs.txt
